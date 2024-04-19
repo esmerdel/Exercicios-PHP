f71c3e12a7ee4cb372ad871ccb886d5c4062bb88
esmerdel <leandro_esmerdel@hotmail.com>
2024-04-19 16:35:13 -0300

    coments

--- lista2/exerc7resposta.php
+++ lista2/exerc7resposta.php
@@ -1,25 +1,38 @@
 <?php
+  // Verifica se o formulário foi submetido
   if(isset($_POST['submit'])){
+    // Inicializa arrays para armazenar alunos aprovados e reprovados
     $aprovados = array();
     $reprovados = array();
+
+    // Itera sobre os alunos submetidos pelo formulário
     foreach ($_POST['alunos'] as $aluno) {
+      // Extrai informações do aluno
       $nome = $aluno['nome'];
       $nota1 = $aluno['nota1'];
       $nota2 = $aluno['nota2'];
+
+      // Calcula a média do aluno
       $media = ($nota1 + $nota2) / 2;
       $aluno['media'] = $media;
+
+      // Verifica se o aluno está aprovado ou reprovado e os adiciona aos respectivos arrays
       if ($media >= 6) {
         $aprovados[] = $aluno;
       } else {
         $reprovados[] = $aluno;
       }
     }
+
+    // Exibe a lista de alunos aprovados
     echo "<p>Lista de aprovados:</p>";
     echo "<ul>";
     foreach ($aprovados as $aluno) {
       echo "<li>{$aluno['nome']} - Média: {$aluno['media']}</li>";
     }
     echo "</ul>";
+
+    // Exibe a lista de alunos reprovados
     echo "<p>Lista de reprovados:</p>";
     echo "<ul>";
     foreach ($reprovados as $aluno) {
